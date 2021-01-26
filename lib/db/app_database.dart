08301a3372e0d02c150318e70de0a6cb25b56bcf
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2021-01-26 16:49:23 -0300

    commit

--- lib/db/app_database.dart
+++ lib/db/app_database.dart
@@ -36,7 +36,7 @@ class AppDatabase {
     // Get a location using path_provider
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
     String path = join(documentsDirectory.path, "ola_mundo_db.db");
-    _database = await openDatabase(path, version: 2,
+    _database = await openDatabase(path, version: 1,
         onCreate: (Database db, int version) async {
       // When creating the db, create the table
       await _criarTabelaUsuario(db);
